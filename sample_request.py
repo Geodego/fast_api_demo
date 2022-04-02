e9de009d5307b13add791086636d601b382969ee
Geodego <geoffroydegournay@hotmail.com>
2022-04-02 16:33:29 +0200

    docs: Comments added to sample_request.py

--- sample_request.py
+++ sample_request.py
@@ -1,9 +1,10 @@
 """
-script used for running the post
+Script used for running the post.
 """
 import requests
 import json
 
+# We define the data we want to pass in
 data = {"name": "Kit", "tags": ["book", "towel"], "item_id": 23}
 
 r = requests.post("http://127.0.0.1:8000/items/", data=json.dumps(data))
